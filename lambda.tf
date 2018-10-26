fd2d5d5284ad31e570c88f81a6d2260d076f7c6e
Ubuntu <ubuntu@ip-172-31-46-223.us-east-2.compute.internal>
2018-10-26 17:04:24 +0000

    Declaritives added

--- lambda.tf
+++ lambda.tf
@@ -9,7 +9,7 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "Demo" {
-  name = "Pipeline-Demo-Final"
+  name = "Pipeline-Demo-Final1"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -28,7 +28,7 @@ EOF
 }
 
 resource "aws_lambda_function" "demo_lambda" {
-    function_name = "basic-pipeline-hello-world"
+    function_name = "basic-pipeline-hello-world1"
     handler = "index.handler"
     runtime = "nodejs6.10"
     filename = "function.zip"
