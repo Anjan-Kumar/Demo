9474b33cb533bed4e98b4150bd871b57fec32363
Ubuntu <ubuntu@ip-172-31-46-223.us-east-2.compute.internal>
2018-10-26 04:10:38 +0000

    changes to lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -9,7 +9,7 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "Demo" {
-  name = "Demo-Final"
+  name = "Pipeline-Demo-Final"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -28,7 +28,7 @@ EOF
 }
 
 resource "aws_lambda_function" "demo_lambda" {
-    function_name = "hello-world-Final"
+    function_name = "basic-pipeline-hello-world"
     handler = "index.handler"
     runtime = "nodejs6.10"
     filename = "function.zip"
