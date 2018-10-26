875970fba355a10cbc57d0fefa2a7a7ff39757fd
bbansal <bbansal@netgear.com>
2018-10-26 08:15:20 +0530

    Latest changes

--- lambda.tf
+++ lambda.tf
@@ -1,6 +1,9 @@
 ################################################################
 #Sample Lambda Function
 ################################################################
+variable "access_key" {}
+variable "secret_key" {}
+
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
